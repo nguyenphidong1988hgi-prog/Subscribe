fc83773f780eb0b7f99fbffbf4ef8afb747b3761
nguyenphidong1988hgi-prog <nguyenphidong1988.hgi@gmail.com>
2026-06-22 18:04:14 +0700

    Add subscription counter to Program

--- Program.cs
+++ Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
+    static int subscribe = 0;
+
     static void Main()
     {
         Console.WriteLine("Subscribe Game");
+
+        subscribe++;
+
+        Console.WriteLine("Subscribe: " + subscribe);
     }
 }
